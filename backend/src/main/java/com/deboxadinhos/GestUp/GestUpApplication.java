efba3717069d2a7acb84fdcadb492b08b0d2d7b7
guilhermedevbr06 <guilherme.dev.br06@gmail.com>
2025-07-31 21:46:17 -0300

    Inicio back

---
+++ backend/src/main/java/com/deboxadinhos/GestUp/GestUpApplication.java
@@ -0,0 +1,13 @@
+package com.deboxadinhos.GestUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestUpApplication.class, args);
+	}
+
+}
