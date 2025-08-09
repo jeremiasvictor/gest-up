9661a69976a98f5dbd60a8183db1d14865ebeac4
Jeremias Santos <jeremiasmarinho05@gmail.com>
2025-08-08 23:30:31 -0300

    improving login page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -3,19 +3,36 @@ import "./style.css";
 function Login() {
   return (
     <div className="container">
+      <div className="bg-icons">
+        <img src="../../../public/flecha.png" alt="" className="bg-arrow" />
+        <img src="../../../public/flecha.png" alt="" className="bg-arrow" />
+      </div>
+
+      <img src="../../../public/icon.png" alt="Logo Image" />
       <h1>Log in to GestUp</h1>
       <form>
         <label htmlFor="username">Username</label>
         <div className="input">
           <input type="text" name="name" />
         </div>
-        <label htmlFor="username">Password</label>
+        <div className="password-items">
+          <label htmlFor="username">Password</label>
+          <label className="forgot" htmlFor="forgot-password">
+            Forgot password?
+          </label>
+        </div>
         <div className="input">
           <input type="text" name="password" />
+          <img
+            src="../../../public/hidden.png"
+            alt="Hidden Icon"
+            className="hidden-icon"
+          />
         </div>
         <button type="button" onClick={() => console.log("botao")}>
           Log in
         </button>
+        ''
       </form>
 
       <div className="toSignIn">
