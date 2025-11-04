6cd372d18138f2e5eb0e9c99c8c02bd2e3a9620d
guilhermedevbr06 <guilherme.dev.br06@gmail.com>
2025-11-04 10:41:46 -0300

    New exceptions

---
+++ backend/src/main/java/com/deboxadinhos/GestUp/exceptions/usuarioException/NullNameException.java
@@ -0,0 +1,7 @@
+package com.deboxadinhos.GestUp.exceptions.usuarioException;
+
+public class NullNameException extends RuntimeException {
+  public NullNameException(String message) {
+    super(message);
+  }
+}
